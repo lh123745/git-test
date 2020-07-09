6d75ef25628197b1535506102b3a7e82ce538494
LiaoHong <759536781@qq.com>
2020-07-09 17:58:42 +0800

    修改git文件

--- git-test-project/src/com/itheima/git.java
+++ git-test-project/src/com/itheima/git.java
@@ -6,9 +6,11 @@ public class git {
 		System.out.println("测试git");
 		System.out.println("测试git");
 		System.out.println("测试git");
+		//hello test
 	}
 	public void test(){
 		int i = 0;
 		i += 10;
+		System.out.println(i);
 	}
 }
