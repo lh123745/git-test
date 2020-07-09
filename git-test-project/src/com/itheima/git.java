af5b96af207f1d45af61c32e222742c064955098
LiaoHong <759536781@qq.com>
2020-07-09 17:40:10 +0800

    添加git.java文件

---
+++ git-test-project/src/com/itheima/git.java
@@ -0,0 +1,10 @@
+package com.itheima;
+
+public class git {
+	public static void main(String[] args) {
+		System.out.println("测试git");
+		System.out.println("测试git");
+		System.out.println("测试git");
+		System.out.println("测试git");
+	}
+}
